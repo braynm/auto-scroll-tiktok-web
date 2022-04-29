fbd2c097c490ca95da26f3b08293c223c1c920f1
Bryan <bryanfmadrid@gmail.com>
2022-04-29 23:16:52 +0800

    WIP

---
+++ content-script.js
@@ -0,0 +1,119 @@
+//const container = document.querySelector('[data-e2e="feed-video"]')
+
+//if (container) {
+  //console.log({
+    //'>>>>>': container.querySelector('video')
+  //})
+//}
+//console.log({ container })
+
+
+
+//const video = document.querySelector('video');
+
+//if (video) {
+  //console.log({ video })
+  //video.onended = function (e) {
+    //alert(1)
+    //const button = document.querySelector('[data-e2e="arrow-right"]');
+    //button.click();
+  //}
+//}
+
+//const button = document.querySelector('[data-e2e="arrow-right"]');
+
+//if (button) {
+  //button.onclick = function() {
+    //console.log('hey..')
+  //}
+
+  //button.click();
+//}
+
+(function(globalSope) {
+    'use strict';
+
+    /**
+     * Including this file adds the `addDynamicEventListener` function to the global scope.
+     * 
+     * The dynamic listener gets an extra `selector` parameter that only calls the callback
+     * if the target element matches the selector.
+     * 
+     * The listener has to be added to the container/root element and the selector must match
+     * the elements that should trigger the event.
+     * 
+     * Browser support: IE11+
+     */
+
+    /**
+     * Returns a modified callback function that calls the
+     * initial callback function only if the target element matches the given selector
+     * 
+     * @param {string} selector 
+     * @param {function} callback 
+     */
+    function getConditionalCalback(selector, callback) {
+        return function(e) {
+            if(e.target && e.target.matches(selector)) {
+                e.delegatedTarget = e.target;
+                callback.apply(this, arguments);
+                return;
+            }
+            // Not clicked directly, check bubble path
+            var path = event.path || (event.composedPath && event.composedPath());
+            if(!path) return;
+            for(var i = 0; i < path.length; ++i) {
+                var el = path[i];
+                if (el.matches(selector)) {
+                    // Call callback for all elements on the path that match the selector
+                    e.delegatedTarget = el;
+                    callback.apply(this, arguments);  
+                }
+                // We reached parent node, stop
+                if (el === e.currentTarget) {
+                    return;
+                }
+            }
+        };
+    }
+
+    /**
+     * 
+     * 
+     * @param {Element} rootElement The root element to add the linster too.
+     * @param {string} eventType The event type to listen for.
+     * @param {string} selector The selector that should match the dynamic elements.
+     * @param {function} callback The function to call when an event occurs on the given selector.
+     * @param {boolean|object} options Passed as the regular `options` parameter to the addEventListener function
+     *                                 Set to `true` to use capture.
+     *                                 Usually used as an object to add the listener as `passive`
+     */
+    globalSope.addDynamicEventListener = function (rootElement, eventType, selector, callback, options) {
+        rootElement.addEventListener(eventType, getConditionalCallback(selector, callback), options);
+    };
+})(this);
+
+
+const videoFromFeed = document.querySelectorAll('div[data-e2e="feed-video"]');
+videoFromFeed.forEach(item => {
+  item.onclick = () => {
+    console.log('lockededededed')
+  }
+})
+
+addDynamicEventListener(document.body, 'click', 'div[data-e2e="feed-video"]', function (e) {
+    console.log('Clicked', e.target.innerText);
+});
+
+//videoFromFeed.onclick = function() {
+  //console.log('clicked..')
+  //const browseVideo = document.querySelector('[data-e2e="browse-video"]')
+  //console.log({
+    //browseVideo,
+    ////browseVideo: browseVideo.querySelector('video')
+  //})
+  ////const browseVideo.onended = function() {
+    ////alert(1)
+  ////}
+  ////console.log({ browseVideo })
+//}
--- popup.js
+++ popup.js
@@ -1,16 +1,37 @@
 // When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+//changeColor.addEventListener("click", async () => {
+  //let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
+  //chrome.scripting.executeScript({
+    //target: { tabId: tab.id },
+    //function: setPageBackgroundColor,
+  //});
+//});
+
+document.onload = function() {
+  alert(1);
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    function: setPageBackgroundColor,
+    files: ['content-script.js']
+    //function: setPageBackgroundColor,
   });
-});
+}
 
 // The body of this function will be executed as a content script inside the
 // current page
 function setPageBackgroundColor() {
+  const video = document.querySelector('video');
+
+  if (video) {
+    console.log({ video })
+    video.onended = function (e) {
+      const button = document.querySelector('[data-e2e="arrow-right"]');
+      button.click();
+    }
+  }
+
+
   chrome.storage.sync.get("color", ({ color }) => {
     document.body.style.backgroundColor = color;
   });
